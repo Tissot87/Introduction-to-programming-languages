d410ccef45366dd1da4f306195a441a5c255a29a
Kirill <kandriyanoff@yandex.ru>
2023-09-25 22:40:10 +0300

    first task

---
+++ HM1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Insert first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("First number is bigger");
+}
+else
+{
+    Console.WriteLine("Second is bigger");
+}
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
