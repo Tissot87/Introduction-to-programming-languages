2a36285ab2601f2fb4c3df885eebd29d1c85e99d
Kirill <kandriyanoff@yandex.ru>
2023-10-21 23:43:11 +0300

    jf

--- Program.cs
+++ Program.cs
@@ -19,21 +19,26 @@
 // M = 4; N = 8. -> 30
 
 
-int SumBetweenNumbers(int m, int n)
+// int SumBetweenNumbers(int m, int n)
 
-{
-    int sum = 0;
-    for (int i = Math.Min(m, n); i <= Math.Max(m, n); i++)
-    {
-        sum = sum + i;
-    }
-    return sum;
-}
+// {
+//     int sum = 0;
+//     for (int i = Math.Min(m, n); i <= Math.Max(m, n); i++)
+//     {
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
 
 
-Console.WriteLine("Enter n");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter m");
-int m = Convert.ToInt32(Console.ReadLine());
-int result = SumBetweenNumbers(m, n);
-Console.WriteLine(result);
+// Console.WriteLine("Enter n");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter m");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int result = SumBetweenNumbers(m, n);
+// Console.WriteLine(result);
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
